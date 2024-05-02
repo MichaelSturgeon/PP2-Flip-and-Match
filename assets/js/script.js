bf61e9c9dee4b7cbb9717427716eeefb52b95ef7
Michael Sturgeon <1993msturgeon@gmail.com>
2024-05-02 21:12:25 +0000

    Create a basic layout with comments to organise JS code

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * The code below ensures that the DOM loads before starting the game. 
+ */
+document.addEventListener("DOMContentLoaded", function() {
+// Start of welcome screen code
+
+// End of welcome screen code
+// Start of game board code
+
+// End of game board code
+// Start of score board code
+
+// End of score board code
+})
